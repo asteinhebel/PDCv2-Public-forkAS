6dd32c21a7f07c8bc850207dc80ef8c6afa09e53
i8x <steinhebelal@ornl.gov>
2025-04-25 13:05:51 -0400

    generalize analysis for arbitrary number of PDCs. If nmbPDC!=4 then assume the csv was created offline and uses comma delimiters

--- hostApps/python/countDropped_testPixQcUsingTrigger.py
+++ hostApps/python/countDropped_testPixQcUsingTrigger.py
@@ -2,11 +2,15 @@ import pandas as pd
 import sys
 import matplotlib.pyplot as plt
 
-df = pd.read_csv(sys.argv[1], sep=';')
+nmbPDCs = 2
+if nmbPDCs<4:
+    df = pd.read_csv(sys.argv[1])
+else:
+    df = pd.read_csv(sys.argv[1], sep=';')
 
 #Get trigger rows
 expectedTrigs = 5
-df_trigs = df[[f'SPAD_TCR{i}' for i in range(4)]]
+df_trigs = df[[f'SPAD_TCR{i}' for i in range(nmbPDCs)]]
 df_droppedTrigs = df_trigs[df_trigs.isin([4]).any(axis=1)]
 
 print(f"Number of QCs that did not record {expectedTrigs} triggers")
@@ -18,7 +22,7 @@ print(df_droppedTrigs.head(-1))
 #plot
 plt.figure()
 s=[100,50,30,10]
-for i in range(4):
+for i in range(nmbPDCs):
     alpha=0.6 if i>1 else 1
     plt.scatter(df_droppedTrigs.index,df_droppedTrigs[f'SPAD_TCR{i}'], label=i, s=s[i])
 plt.legend(loc='best')
