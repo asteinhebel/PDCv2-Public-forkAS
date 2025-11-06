0f25a2ef3594ef35b4a40356049b7ed74b12494d
rost1501 <tommy.rossignol@usherbrooke.ca>
2025-11-06 15:13:33 -0500

    added head ID, histogram bins are side by side and y axis is log, some changes in display and form factors

--- hostApps/python/Display_Head2x2_TCR.py
+++ hostApps/python/Display_Head2x2_TCR.py
@@ -23,35 +23,12 @@ from matplotlib.colors import *
 import math
 import matplotlib.lines as mlines
 import argparse
+from modules.pixMap import xy_map
 
 
 
-def reverse_idx_map(idx: int):
-    """
-    Reverse the idx_map function to convert a 1D pixel index back to (x, y) coordinates.
-
-    Args:
-        idx (int): The 1D pixel index.
-
-    Returns:
-        tuple[int, int]: The (x, y) coordinates.
-    """
-    # Initialize x and y
-    for y in range(64):
-        idx_y = (y << 2) + 15 * ((y & 0x0000003C) << 2)
-        if idx < idx_y:
-            continue
-        for x in range(64):
-            idx_x = x + 3 * (x & 0x0000003C)
-            if idx == idx_x + idx_y:
-                return x, y
-
-    raise ValueError("Invalid index: Could not map to (x, y)")
-
-
-
-def plot_results_df(df: pd.DataFrame, criteria="SPAD_TCR (cps)"):
-    fig, axes = plt.subplots(2, 4, figsize=(12, 8))
+def plot_results_df(df: pd.DataFrame, criteria="SPAD_TCR (cps)", head_id=None):
+    fig, axes = plt.subplots(2, 4, figsize=(14, 8), width_ratios=[1, 1, 1.5, 1.5])
 
     palette="deep"
 
@@ -72,11 +49,11 @@ def plot_results_df(df: pd.DataFrame, criteria="SPAD_TCR (cps)"):
 
     # Plot sorted population
     ax_pop = sns.ecdfplot(data=df, y=criteria, hue="iPDC", palette=palette, ax=axes[0,1], legend=False)
-    labels = []
+    #labels = []
     for iPDC, df_pdc in df.groupby("iPDC"):
         ax_pop.axhline(y=df_pdc[criteria].median(), color=sns.color_palette(palette)[iPDC], linestyle="--")
         ax_pop.axhline(y=df_pdc[criteria].mean(), color=sns.color_palette(palette)[iPDC], linestyle="-."  )
-        labels.append(f'PDC{iPDC}, med.: {int(df_pdc[criteria].median())}, mean: {int(df_pdc[criteria].mean())}')
+        #labels.append(f'PDC{iPDC}, med.: {int(df_pdc[criteria].median())}, mean: {int(df_pdc[criteria].mean())}')
     ax_pop.set_yscale("log")
     #ax_pop.legend(labels=labels)
 
@@ -86,15 +63,18 @@ def plot_results_df(df: pd.DataFrame, criteria="SPAD_TCR (cps)"):
         axi.remove()
     ax_merged = fig.add_subplot(gs[1, :2])
     df_histo = df[df[criteria].between(10, df[criteria].mean())]
-    ax_merged = sns.histplot(data=df_histo, x=criteria, palette=palette, hue="iPDC", ax=ax_merged, kde=True, bins=40)
+    ax_merged = sns.histplot(data=df_histo, x=criteria, palette=palette, hue="iPDC",
+                             ax=ax_merged, kde=True, bins=40, multiple="dodge", log_scale=False)
     ax_merged.set_xscale("log")
+    ax_merged.set_yscale("log")
+    ax_merged.set_ylim(ymin=1)
 
 
     # Plot heatmaps
     heatmap_loc = [(0,2), (1,2) ,(1,3), (0,3)]
 
-    df[['X', 'Y']] = df['SPAD_idx'].apply(reverse_idx_map).apply(pd.Series)
-    df["TCR_log"] =  np.log10(df[criteria])
+    df[['X', 'Y']] = df['SPAD_idx'].apply(xy_map).apply(pd.Series)
+    #df["TCR_log"] =  np.log10(df[criteria])
 
     for iPDC, df_pdc in df.groupby("iPDC"):
         ax = axes[heatmap_loc[iPDC]]
@@ -102,7 +82,8 @@ def plot_results_df(df: pd.DataFrame, criteria="SPAD_TCR (cps)"):
         df_pdc = df_pdc[df_pdc[criteria] < 500_000]
         df_hm = df_pdc.pivot_table(index='Y', columns='X', values=criteria)
         
-        ax = sns.heatmap(data=df_hm, square=True, clip_on=False, xticklabels=10,yticklabels=10, robust=False, cmap="viridis", norm=LogNorm(), ax=ax)
+        ax = sns.heatmap(data=df_hm, square=True, clip_on=False, xticklabels=10,yticklabels=10,
+                         robust=False, cmap="viridis", norm=LogNorm(), ax=ax, cbar_kws={"fraction":0.045})
         ax.invert_yaxis()
 
     items = []
@@ -115,7 +96,10 @@ def plot_results_df(df: pd.DataFrame, criteria="SPAD_TCR (cps)"):
     axes[0,0].legend(handles=items, bbox_to_anchor=(0., 1.02, 3.2, .102), loc=3,
                ncol=2,  borderaxespad=0, )
     #fig.legend(handles=items, ncols=4, loc="lower right")
-
+    plt.subplots_adjust(left=0.05, bottom=0.08, right=0.96, top=0.92, wspace=0.3, hspace=0.2)
+    #fig.tight_layout()
+    if head_id is not None:
+        fig.text(x=0.70, y=0.95, s=head_id, fontsize=20)
 
     return fig
 
@@ -135,8 +119,12 @@ def main():
     if args.file:
         # Get the file name using the .name attribute
         file_name = args.file.name
+        print(file_name)
+        head_id = file_name.split('_')[3]
+        if head_id[0] != "H":
+            head_id = None
         df = csv_to_df(args.file.name)
-        fig = plot_results_df(df, criteria="SPAD_TCR (cps)")
+        fig = plot_results_df(df, criteria="SPAD_TCR (cps)", head_id=head_id)
     plt.show()
     return
 
@@ -158,6 +146,7 @@ def csv_to_df(filepath:str):
         df_pdc["SPAD_TCR (cps)"] = data[f"SPAD_TCR{iPDC}"]
         df_pdc["iPDC"] = iPDC
         df = pd.concat([df, df_pdc], ignore_index=True)
+        print(f"PDC{iPDC} has {(df_pdc['SPAD_TCR (cps)'] == 0).sum()} SPADs with no TCR value")
 
 
     spad_active_area= 42**2-(28**2-math.pi*14**2) #* 1e-6
