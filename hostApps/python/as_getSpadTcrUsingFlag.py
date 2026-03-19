84bcb1adb95799a5301ce82cdf97959b5943fe08
Amanda Steinhebel <21068745+asteinhebel@users.noreply.github.com>
2026-03-19 13:06:32 -0400

    Add automated power supply control

--- hostApps/python/as_getSpadTcrUsingFlag.py
+++ hostApps/python/as_getSpadTcrUsingFlag.py
@@ -21,15 +21,14 @@
 import sys, os
 import numpy as np
 import random
-import time
-import datetime
+import time, datetime
 from itertools import chain
 import statistics
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mticker
 import warnings
 import threading
-import yaml
+import yaml, pyvisa, glob
 
 # custom modules
 from modules.fgColors import fgColors
@@ -41,7 +40,6 @@ from modules.zynqCtlPdcRoutines import initCtlPdcFromClient, packetBank
 from modules.zynqDataTransfer import zynqDataTransfer
 from modules.systemHelper import sectionPrint
 from modules.pdcHelper import *
-#from modules.zynqHelper import *
 from modules.h5Reader import *
 
 
@@ -293,6 +291,88 @@ elif method == ZppModuleSetMethod.registers:
     client.runPrint(f"ctlCfg -a ZPL0 -r 0x{ZPP_LOW_REG&0xFFFF:04x} -g")
     client.runPrint(f"ctlCfg -a ZPL1 -r 0x{(ZPP_LOW_REG>>16)&0xFFFF|0x8000:04x} -g")  # |0x8000 to enable ZPP
 
+# ---------------------------------------
+# --- ORNL SPECIFIC - Set up power supply settings
+# ---------------------------------------
+
+# BK Precision to supply -250V substrate voltage, resource_bkps
+# Keysight to supply -25V SPAD bias voltage, resource_ps
+def checkSystHealth(healthbytes):
+
+    if healthbytes=="000000" or healthbytes=="000001":
+        #Require all systems off, including over-current and over-voltage protections
+        print('All good')
+        return 
+    elif healthbytes=="000009": #output is on
+        print("Output is on - turn off to begin")
+        inst_bkps.write("OUT OFF")
+        return
+    elif healthbytes=="0000A1": #ovp and ovc still set
+        print("Over-current and -voltage protections are set - turn off to begin")
+        inst_bkps.write("PROT:OCP OFF")
+        inst_bkps.write("PROT:OVP OFF")
+        return
+    elif healthbytes=="0000A9": #output, ovp and ovc still set
+        print("Settings remain from previous run - turn off to begin")
+        inst_bkps.write("OUT OFF")
+        inst_bkps.write("PROT:OCP OFF")
+        inst_bkps.write("PROT:OVP OFF")
+        return
+    else:
+        print(f"Failed system health: {systHealth}")
+        sys.exit()
+
+###Identify supplies
+rm = pyvisa.ResourceManager('@py')
+serialID_bkps = glob.glob('/dev/serial/by-id/*CP2102*')[0]
+serialID_dcps = glob.glob('/dev/serial/by-id/*Prolific*')[0]
+resource_bkps = f"ASRL/dev/{os.readlink(serialID_bkps)[-7:]}::INSTR"
+resource_dcps = f"ASRL/dev/{os.readlink(serialID_dcps)[-7:]}::INSTR"
+
+foundResources = rm.list_resources()
+if resource_bkps not in foundResources:
+    print('Could not find BK device in expected location - try unplugging')
+    sys.exit()
+if resource_dcps not in foundResources:
+    print('Could not find Keysight device in expected location - try unplugging')
+    sys.exit()
+
+###Set up BK Precision to supply substrate voltage
+print('opening resource: ' + resource_bkps)
+inst_bkps = rm.open_resource(resource_bkps, write_termination = '\n',read_termination='\r\n',baud_rate=57600)
+print(f'BKPS Device: {inst_bkps.query("*IDN?")}')
+
+#clear status and errors
+#inst_bkps.write("*CLS")
+systHealth = inst_bkps.query("STATUS?")[:6]
+checkSystHealth(systHealth)
+
+#Set over current and over voltage protections
+inst_bkps.write("PRO:OCP:LEV 0.00005") #50 uA current limit
+inst_bkps.write("PRO:OVP:LEV 260.0") #260 V voltage limit
+print('Set over-current and over-voltage protection limits')
+inst_bkps.write("PROT:OCP ON")
+inst_bkps.write("PROT:OVP ON")
+
+###Set up Keysight to supply bias voltage
+print('opening resource: ' + resource_dcps)
+inst_dcps = rm.open_resource(resource_dcps, write_termination = '\n',read_termination='\n',baud_rate=9600)
+print(f'DCPS Device: {inst_dcps.query("*IDN?")}')
+systHealth = inst_dcps.query("*TST?")
+if '0' in systHealth:
+    print('All good')
+    inst_dcps.write("SYST:BEEP")
+else:
+    print(f"Failed system health: {systHealth}")
+    sys.exit()
+
+#Ensure that the output is not on while setting proper values
+if int(inst_dcps.query("OUTP?")) == 1:
+    print("output is on - turn it off")
+    inst_dcps.write("OUTP OFF")
+#Set over current and over voltage protections
+inst_dcps.write("VOLT:PROT 25.5")
+
 
 # ---------------------------------------
 # --- Notify user of manual steps
@@ -300,6 +380,32 @@ elif method == ZppModuleSetMethod.registers:
 try:
     print(f"{fgColors.bYellow}Apply HV here{fgColors.endc}")
     if os.environ.get("BATCH_MODE") is None:
+        #ORNL SPECIFIC - turn on power supplies
+        input("Apply substrate bias?")
+        #Ramp output voltage to 250 in 10V steps
+        inst_bkps.write("OUT ON")
+
+        print("BK HV Ramping up....")
+        for vUp in range(10, 250+1, 10):
+            time.sleep(0.5)
+            if vUp==10: #begin ramp slowly
+                for vUpFine in range(10):
+                    inst_bkps.write(f"SOUR:VOLT {float(vUpFine)}")
+                    time.sleep(0.5)
+            inst_bkps.write(f"SOUR:VOLT {float(vUp)}")
+        print("BK HV is supplied")
+
+        input("Apply SPAD bias?")
+        #Ramp output voltage to 25 in 1V steps
+        inst_dcps.write("APPL 0.0, 0.0")
+        inst_dcps.write("OUTP ON")
+
+        print("Keysight HV Ramping up....")
+        for vUp in range(25+1):
+            time.sleep(0.5)
+            inst_dcps.write(f"APPL {float(vUp)}, 0.1")
+        print("Keysight HV is supplied")
+
         input("Press [enter] key to continue")
 except KeyboardInterrupt:
     print("\nKeyboard Interrupt: exit program")
@@ -1099,6 +1205,31 @@ except (KeyboardInterrupt, SystemExit) as ex:
         print(f"\n{fgColors.yellow}Program interrupted: exit program{fgColors.endc}")
 
 finally:
+    # ORNL SPECIFIC - turn off power 
+    print("Turn off power supplies")
+    print("Keysight ramping down....")
+    for vDown in range(25,-1,-1):
+        time.sleep(0.5)
+        inst_dcps.write(f"APPL {float(vDown)}, 0.1")
+    print("Keysight HV is turned off")
+
+    print(f"Run over - turn off Keysight output")
+    inst_dcps.write("APPL 0.0, 0.0")
+    inst_dcps.write("OUTP OFF")
+    inst_dcps.close()
+    print("BK ramping down....")
+    for vDown in range(250,-1,-10):
+        time.sleep(0.5)
+        inst_bkps.write(f"SOUR:VOLT {float(vDown)}")
+    print("BK HV is turned off")
+
+    print(f"Run over - turn off BK output")
+    inst_bkps.write("SOUR:VOLT 0.0")
+    inst_bkps.write("OUT OFF")
+    inst_bkps.write("PROT:OCP OFF")
+    inst_bkps.write("PROT:OVP OFF")
+    inst_bkps.close()
+
     if not 'test_stop_time' in locals():
         test_stop_time = time.time()
         print(f"{fgColors.bBlue}Test took {test_stop_time-test_start_time:.3f} seconds{fgColors.endc}")
